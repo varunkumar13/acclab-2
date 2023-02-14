581ea49a90e2c469af255db351468b53f8058cb7
varunkumar13 <varunkumaraun@gmail.com>
2023-02-14 12:43:53 +0530

    acclab-2

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Demo2 from "./Demo2";
 
 const Demo = (props) => {
-    return <div> Demo component
+    return <div>
+    <Demo2 para2={props.para}/>
+     Demo component
     <h1> {props.para}</h1>
     </div>
 
---
+++ src/components/Demo2.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Demo2 = (props) => {
+    return <div> Demo2 component, {props.para2}
+   
+    </div>
+
+};
+export default Demo2;
